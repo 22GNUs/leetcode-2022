c666db7a0e79fb00ab876228fb5715b73a787692
22GNUs <stormaroon@gmail.com>
2022-11-19 23:55:00 +0800

    Daily exercise

---
+++ 94.二叉树的中序遍历.ts
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=typescript
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (76.10%)
+ * Likes:    1621
+ * Dislikes: 0
+ * Total Accepted:    1M
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    const ret = new Array<number>()
+    function traversal(node: TreeNode | null) {
+        if (!node) return
+        if (node.left) traversal(node.left)
+        ret.push(node.val)
+        if (node.right) traversal(node.right)
+    }
+    traversal(root)
+    return ret
+};
+// @lc code=end
+
