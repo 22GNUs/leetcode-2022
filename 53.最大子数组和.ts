9175484cb37a76b06d73be37671e8d4134539a1b
22GNUs <stormaroon@gmail.com>
2022-11-23 23:06:34 +0800

    Daily exercise

---
+++ 53.最大子数组和.ts
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=typescript
+ *
+ * [53] 最大子数组和
+ *
+ * https://leetcode.cn/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Medium (54.86%)
+ * Likes:    5467
+ * Dislikes: 0
+ * Total Accepted:    1.2M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 
+ * 子数组 是数组中的一个连续部分。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1]
+ * 输出：1
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [5,4,-1,7,8]
+ * 输出：23
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -10^4 <= nums[i] <= 10^4
+ * 
+ * 
+ * 
+ * 
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ * 
+ */
+
+// @lc code=start
+function maxSubArray(nums: number[]): number {
+    const dp = new Array<number>(nums.length)
+    dp[0] = nums[0]
+    let max = dp[0]
+    for (let i = 1; i < nums.length; i++) {
+        // 要么自成一派，要么和前面的子数组合并
+        dp[i] = Math.max(nums[i], dp[i - 1] + nums[i])
+        max = Math.max(max, dp[i])
+    }
+    return max
+};
+
+// @lc code=end
+
